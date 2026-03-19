d1f6b5095d471b0b350c5352ddead78263bce15f
UmeshDesaboina <149916679+UmeshDesaboina@users.noreply.github.com>
2026-03-19 14:12:39 +0530

    Switch to maintenance mode HTML
    Replaced React rendering with a maintenance HTML page.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './App.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<!-- maintenance.html -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Maintenance</title>
+</head>
+<body style="text-align:center; margin-top:100px;">
+  <h1>🚧 We'll be back soon!</h1>
+  <p>Our website is under maintenance. Please check again later.</p>
+</body>
+</html>
